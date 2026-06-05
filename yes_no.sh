2ce81e733f627579edaba61cfe211c2e58a3f631
Shruti Mandaokar <shruti.mandaokar@gmail.com>
2026-06-05 08:09:52 -0700

    Create yes_no.sh to handle yes/no input
    Add a script to read a character and respond with YES or NO based on input.

---
+++ yes_no.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Question:
+# Read in one character from STDIN.
+# If the character is 'Y' or 'y' display "YES".
+# If the character is 'N' or 'n' display "NO".
+#
+# Input Format:
+# One character
+#
+# Constraints:
+# The character will be from the set {Y, y, N, n}.
+#
+# Output Format:
+# echo YES or NO to STDOUT.
+#
+# Sample Input:
+# y
+#
+# Sample Output:
+# YES
+
+# Answer:
