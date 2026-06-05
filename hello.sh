28628ebedc5d54b2202ce7895e36cf75f2aeef8e
Shruti Mandaokar <shruti.mandaokar@gmail.com>
2026-06-05 07:10:17 -0700

    Create hello.sh to print 'HELLO'
    Add a bash script that prints 'HELLO'.

---
+++ hello.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Question:
+# Write a bash script that prints the string "HELLO".
+#
+# Input Format:
+# There is no input file required for this problem.
+#
+# Output Format:
+# HELLO
+#
+# Sample Input:
+# -
+#
+# Sample Output:
+# HELLO
+
+# Answer:
+echo "HELLO"
